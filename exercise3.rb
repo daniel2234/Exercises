649316bccea30accf274c6dcddec2dcf25052500
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2014-08-12 17:12:43 -0400

    Added age input and year born output

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,14 @@
+
 puts "What is your name?"
 
 name = gets.chomp
 
 puts "Hi #{name}!"
+
+puts "What is your age?"
+
+age = gets.chomp.to_i
+
+year= Time.now.year
+
+puts "Hi #{name} you were born in #{year-age}!"
